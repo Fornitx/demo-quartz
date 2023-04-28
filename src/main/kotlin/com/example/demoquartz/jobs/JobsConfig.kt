b6b6dacd0846dce59308999ab5b2d0a3261febcb
Fornitx <49841725+Fornitx@users.noreply.github.com>
2023-04-28 18:22:58 +0300

    simplification

--- src/main/kotlin/com/example/demoquartz/jobs/JobsConfig.kt
+++ src/main/kotlin/com/example/demoquartz/jobs/JobsConfig.kt
@@ -13,14 +13,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
 
-private inline fun <reified T : Job> newJobDetail(name: String) = JobBuilder
+private inline fun <reified T : Job> newJobDetail(name: String): JobDetail = JobBuilder
     .newJob()
     .ofType(T::class.java)
     .storeDurably()
     .withIdentity(name)
     .build()
 
-private fun newTrigger(job: JobDetail, name: String, interval: Duration) = TriggerBuilder
+private fun newTrigger(job: JobDetail, name: String, interval: Duration): Trigger = TriggerBuilder
     .newTrigger()
     .forJob(job)
     .withIdentity(name)
@@ -38,47 +38,33 @@ class JobsConfig {
     @ConditionalOnProperty("$PREFIX.jobs.job1.enabled", havingValue = "true", matchIfMissing = false)
     class Job1Config {
         @Bean
-        fun jobDetail1(): JobDetail {
-            return newJobDetail<DemoJob1>("Qrtz_JobDetail1")
-        }
+        fun jobDetail1() = newJobDetail<DemoJob1>("Qrtz_JobDetail1")
 
         @Bean
-        fun demoService1(): DemoService1 {
-            return DemoService1()
-        }
+        fun demoService1() = DemoService1()
 
         @Bean
-        fun demoJob1(service: DemoService1): DemoJob1 {
-            return DemoJob1(service)
-        }
+        fun demoJob1(service: DemoService1) = DemoJob1(service)
 
         @Bean
-        fun trigger1(@Qualifier("jobDetail1") job: JobDetail, properties: DemoProperties): Trigger {
-            return newTrigger(job, "Qrtz_Trigger1", properties.jobs.job1.interval)
-        }
+        fun trigger1(@Qualifier("jobDetail1") job: JobDetail, properties: DemoProperties) =
+            newTrigger(job, "Qrtz_Trigger1", properties.jobs.job1.interval)
     }
 
     @Configuration
     @ConditionalOnProperty("$PREFIX.jobs.job2.enabled", havingValue = "true", matchIfMissing = false)
     class Job2Config {
         @Bean
-        fun jobDetail2(): JobDetail {
-            return newJobDetail<DemoJob2>("Qrtz_JobDetail2")
-        }
+        fun jobDetail2() = newJobDetail<DemoJob2>("Qrtz_JobDetail2")
 
         @Bean
-        fun demoService2(): DemoService2 {
-            return DemoService2()
-        }
+        fun demoService2() = DemoService2()
 
         @Bean
-        fun demoJob2(service: DemoService2): DemoJob2 {
-            return DemoJob2(service)
-        }
+        fun demoJob2(service: DemoService2) = DemoJob2(service)
 
         @Bean
-        fun trigger2(@Qualifier("jobDetail2") job: JobDetail, properties: DemoProperties): Trigger {
-            return newTrigger(job, "Qrtz_Trigger2", properties.jobs.job2.interval)
-        }
+        fun trigger2(@Qualifier("jobDetail2") job: JobDetail, properties: DemoProperties) =
+            newTrigger(job, "Qrtz_Trigger2", properties.jobs.job2.interval)
     }
 }
